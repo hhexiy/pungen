21d816fcedaf9f8727146a731a27ebec7fa0e38c
violetpeng <npeng@isi.edu>
2018-11-03 21:39:49 -0700

    add preprocess for fw and bw baseline model

---
+++ scripts/preprocess_forward_backward.py
@@ -0,0 +1,76 @@
+import sys, os
+import random
+
+def load_keywords(keyset, infile):
+    with open(infile) as inf:
+        for line in inf:
+            keyset.add(line.strip())
+    return keyset
+
+def get_vocab(infile):
+    vocab_dict = dict()
+    with open(infile) as inf:
+        for line in inf:
+            elems = line.strip().split()
+            for el in elems:
+                if el not in vocab_dict:
+                    vocab_dict[el] = 0
+                vocab_dict[el] += 1
+    return vocab_dict
+
+def process_file_fb(infile, keyset, vocab, max_sample=3, threshold=10):
+    dirname = os.path.dirname(infile)
+    if not os.path.exists(os.path.join(dirname, '1backward')):
+        os.mkdir(os.path.join(dirname, '1backward'))
+    if not os.path.exists(os.path.join(dirname, '2forward')):
+        os.mkdir(os.path.join(dirname, '2forward'))
+    with open(infile) as inf, open(os.path.join(dirname, '1backward', 'train.in'), 'w') as bsout, \
+            open(os.path.join(dirname, '1backward', 'train.out'), 'w') as btout, \
+            open(os.path.join(dirname, '2forward', 'train.in'), 'w') as fsout, \
+            open(os.path.join(dirname, '2forward', 'train.out'), 'w') as ftout :
+        for line in inf:
+            elems = line.strip().split()
+            kwset = set(elems) & keyset
+            idxs = [elems.index(kw) for kw in kwset]
+            if len(idxs) < max_sample:
+                try:
+                    sid = random.sample(set(range(len(elems)/2, len(elems)-1))-set(idxs), max_sample - len(idxs))
+                except:
+                    continue
+                idxs.extend(sid)
+            write_files(elems, idxs, vocab, bsout, btout, fsout, ftout, threshold)
+    
+def write_files(sentence, idxs, vocab, bsout, btout, fsout, ftout, threshold=10):
+    conv_sent = [word if vocab[word] >= threshold else '<unk>' for word in sentence]
+    for idx in idxs:
+        bsout.write(conv_sent[idx] + '\n')
+        btout.write(' '.join(conv_sent[idx-1::-1]) + '\n')
+        fsout.write(' '.join(conv_sent[:idx+1]) + '\n')
+        ftout.write(' '.join(conv_sent[idx+1:]) + '\n')
+
+
+def print_vocab(outfile, vocab, threshold=10):
+    counter = 0
+    with open(outfile, 'w') as outf:
+        outf.write('<unk>\n')
+        outf.write('<s>\n')
+        outf.write('</s>\n')
+        for k, v in vocab.items():
+            if v >= threshold:
+                outf.write(k + '\n')
+                counter += 1
+        print('effective vocab size:', counter)
+
+
+if __name__ == '__main__':
+    infile = sys.argv[1]
+    keyword_set = set()
+    load_keywords(keyword_set, sys.argv[2])
+    print('keyword size:', len(keyword_set))
+    load_keywords(keyword_set, sys.argv[3])
+    print('keyword size:', len(keyword_set))
+    vocab_dict = get_vocab(infile) 
+    print('vocab size:', len(vocab_dict))
+    threshold=int(sys.argv[4])
+    print_vocab('vocab.in', vocab_dict, threshold)
+    process_file_fb(infile, keyword_set, vocab_dict, max_sample=1, threshold=threshold)
